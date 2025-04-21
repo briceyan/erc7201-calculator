8cef14e2c8404cf9df7ab06da0f14adefc6c86c8
Brice <briceyan3.0@gmail.com>
2025-04-21 10:16:21 +0800

    Restruture to make it deployable to vercel

--- api/index.py
+++ api/index.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request
-from .erc7201 import erc7201, format_solidity
+from utils.erc7201 import erc7201, format_solidity
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="../templates")
 
 
 @app.route("/")
 def index():
-    tmpl = (
-        "index.html"
-        if is_browser(request.user_agent.string)
-        else "index.text"
-    )
+    tmpl = "index.html" if is_browser(request.user_agent.string) else "index.text"
     return render_template(tmpl, host_url=request.host_url)
 
 
@@ -18,11 +14,7 @@ def index():
 def calculate(namespace):
     slot = erc7201(namespace)
     solidity_code = format_solidity(namespace, slot)
-    tmpl = (
-        "result.html"
-        if is_browser(request.user_agent.string)
-        else "result.text"
-    )
+    tmpl = "result.html" if is_browser(request.user_agent.string) else "result.text"
     return render_template(
         tmpl, namespace=namespace, slot=slot, solidity_code=solidity_code
     )
